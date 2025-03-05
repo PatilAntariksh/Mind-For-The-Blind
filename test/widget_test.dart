bfc033edee294eab424a497e3de4b4c80afcbc0a
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-04 20:39:13 -0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:Mind-For-The-Blind/main.dart';  
-import 'package:Mind-For-The-Blind/screens/intro_page.dart';
-import 'package:Mind-For-The-Blind/screens/welcome_page.dart';
-import 'package:Mind-For-The-Blind/screens/signup_page.dart';
-import 'package:Mind-For-The-Blind/screens/login_page.dart';
-import 'package:Mind-For-The-Blind/screens/mode_selection.dart';
+import 'package:mind_for_the_blind/main.dart';  
+import 'package:mind_for_the_blind/screens/intro_page.dart';
+import 'package:mind_for_the_blind/screens/welcome_page.dart';
+import 'package:mind_for_the_blind/screens/signup_page.dart';
+import 'package:mind_for_the_blind/screens/login_page.dart';
+import 'package:mind_for_the_blind/screens/mode_selection.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();

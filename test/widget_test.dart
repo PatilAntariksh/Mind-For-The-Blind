787fdbb1819febe0e9dcfc6d7efb58a24e10ca2c
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 17:20:31 -0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mind_for_the_blind/main.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:mind_for_the_blind/screens/intro_page.dart';
 import 'package:mind_for_the_blind/screens/welcome_page.dart';
 import 'package:mind_for_the_blind/screens/signup_page.dart';

87bd83b577f42c993bc0a15c26013a17c297b53a
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 17:27:19 -0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:mind_for_the_blind/main.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:mind_for_the_blind/screens/intro_page.dart';
 import 'package:mind_for_the_blind/screens/welcome_page.dart';
 import 'package:mind_for_the_blind/screens/signup_page.dart';
@@ -14,8 +14,6 @@ import 'firebase_mock.dart';
 void main() {
   setUpAll(() async {
     await initializeMockFirebase();
-    FirebaseAuth.instanceFor(app: Firebase.app()); 
-    FirebaseFirestore.instance; 
   });
 
   testWidgets("App starts at IntroPage and navigates to WelcomePage", (WidgetTester tester) async {

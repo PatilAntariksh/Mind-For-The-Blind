b0b93b235f354f84cb1ab7be130b8085944a2728
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 16:30:50 -0500

    Update firebase_mock.dart

--- test/firebase_mock.dart
+++ test/firebase_mock.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
 
 void setupFirebaseMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();

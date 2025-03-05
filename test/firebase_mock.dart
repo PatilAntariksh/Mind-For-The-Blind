7c0767768c8f3ea4f1ef7d0171bf323ac10e81d1
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 14:08:11 -0500

    Update firebase_mock.dart

--- test/firebase_mock.dart
+++ test/firebase_mock.dart
@@ -1,17 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_core_platform_interface/firebase_core_platform_interface.dart';
-import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class MockFirebase extends Mock implements Firebase {}
+
+class MockFirebaseCore extends FirebasePlatform {
+  @override
+  Future<FirebaseApp> initializeApp({
+    String? name,
+    FirebaseOptions? options,
+  }) async {
+    return FirebaseApp.instance;
+  }
+}
+
 
 void setupFirebaseMocks() {
   TestWidgetsFlutterBinding.ensureInitialized();
-  
-  setupFirebaseCoreMocks();  // Mocks Firebase core methods
+  FirebasePlatform.instance = MockFirebaseCore();  // Use the mock implementation
 }
+
 
 Future<void> initializeMockFirebase() async {
   setupFirebaseMocks();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp();  
 }

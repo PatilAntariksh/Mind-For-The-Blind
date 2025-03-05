7918ce6e49344eabaea7bfd1cbf7b3a65eedf7bd
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 16:50:32 -0500

    Update firebase_auth_mock.dart

--- test/firebase_auth_mock.dart
+++ test/firebase_auth_mock.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
-import 'package:flutter_test/flutter_test.dart';
 
-void setupFirebaseAuthMocks() {
-  final mockAuth = MockFirebaseAuth();
-  FirebaseAuth.instanceFor(app: Firebase.app()) ?? mockAuth;
+/// Returns a mock instance of FirebaseAuth to be used in tests.
+FirebaseAuth mockFirebaseAuth() {
+  return MockFirebaseAuth();
 }

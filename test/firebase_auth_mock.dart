729e37808b93ec60114836b10c4ac6399d808422
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-03-05 17:00:02 -0500

    Update firebase_auth_mock.dart

--- test/firebase_auth_mock.dart
+++ test/firebase_auth_mock.dart
@@ -1,7 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore_mocks/cloud_firestore_mocks.dart';
 
-/// Returns a mock instance of FirebaseAuth to be used in tests.
+/// Mock FirebaseAuth instance for testing
 FirebaseAuth mockFirebaseAuth() {
   return MockFirebaseAuth();
+}
+
+/// Mock Firestore instance for testing
+FirebaseFirestore mockFirestore() {
+  return MockFirestoreInstance();
 }

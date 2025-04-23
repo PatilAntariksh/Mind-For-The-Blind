050f4be545d4a7f6eb999aacb01b6e2b9465258c
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-23 17:36:40 -0400

    Update video_call.dart

--- lib/screens/video_call.dart
+++ lib/screens/video_call.dart
@@ -12,8 +12,8 @@ class VideoCallPage extends StatelessWidget {
     final String userId = 'user_${DateTime.now().millisecondsSinceEpoch}';
 
     return ZegoUIKitPrebuiltCall(
-      appID: 890795783, // ✅ Your real App ID
-      appSign: '2bf1885667a1def2296bc2be92ce76588286aa2bf900295e0bf4541d399a15d6', // ✅ Your real App Sign
+      appID: 890795783, 
+      appSign: '2bf1885667a1def2296bc2be92ce76588286aa2bf900295e0bf4541d399a15d6', 
       userID: userId,
       userName: 'User_$userId',
       callID: roomId,

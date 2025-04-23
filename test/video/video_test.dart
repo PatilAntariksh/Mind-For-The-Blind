da7ebdd6722ef3fd9bc8c5c3972ad32b29f13b9c
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-23 19:49:00 -0400

    Update video_test.dart

--- test/video/video_test.dart
+++ test/video/video_test.dart
@@ -1,10 +1,17 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:capstone_project/screens/video_call.dart';
 
 void main() {
   testWidgets('Video Call screen loads', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: VideoCallPage()));
+    const testRoomId = 'test-room-123'; //  dummy room ID for test
+
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: VideoCallPage(roomId: testRoomId),
+      ),
+    );
+
     expect(find.text('Video Call'), findsOneWidget);
   });
 }

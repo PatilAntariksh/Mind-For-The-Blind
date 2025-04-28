41da5e307bba9b4071bbc3da93ec120e6a00c85c
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:59:32 -0400

    Update mode_selection_test.dart

--- test/navigation/mode_selection_test.dart
+++ test/navigation/mode_selection_test.dart
@@ -1,14 +1,12 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:capstone_project/screens/mode_selection.dart';
-import '../mocks/mock_services.dart';
 
 void main() {
-  testWidgets('Mode Selection screen shows all mode buttons', (tester) async {
-    final mockTts = MockFlutterTts();
-
+  testWidgets('Mode selection screen shows all buttons', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: ModeSelection()));
 
+    expect(find.text('Choose Your Mode'), findsOneWidget);
     expect(find.text('Back'), findsOneWidget);
     expect(find.text('Video Navigation'), findsOneWidget);
     expect(find.text('Currency Detection'), findsOneWidget);

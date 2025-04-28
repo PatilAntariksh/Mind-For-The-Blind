43a3a47b2dbc791514b20ee1e21371eca5f6c8f3
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 18:45:13 -0400

    Update mode_selection_test.dart

--- test/navigation/mode_selection_test.dart
+++ test/navigation/mode_selection_test.dart
@@ -1,20 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
 import 'package:capstone_project/screens/mode_selection.dart';
-import 'package:fake_async/fake_async.dart';
 
 void main() {
   testWidgets('Mode selection screen shows all buttons', (WidgetTester tester) async {
-    FakeAsync().run((async) async {
-      await tester.pumpWidget(const MaterialApp(home: ModeSelection()));
+    await tester.pumpWidget(const MaterialApp(home: ModeSelection()));
 
-      async.elapse(const Duration(milliseconds: 500)); // let TTS timer complete
-      await tester.pump();
+    await tester.pump(const Duration(milliseconds: 500)); // let TTS timer complete
 
-      expect(find.text('Back'), findsOneWidget);
-      expect(find.text('Video Navigation'), findsOneWidget);
-      expect(find.text('Currency Detection'), findsOneWidget);
-      expect(find.text('AI-Assistant'), findsOneWidget);
-    });
+    expect(find.text('Back'), findsOneWidget);
+    expect(find.text('Video Navigation'), findsOneWidget);
+    expect(find.text('Currency Detection'), findsOneWidget);
+    expect(find.text('AI-Assistant'), findsOneWidget);
   });
 }

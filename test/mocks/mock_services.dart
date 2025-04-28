e7cd7c71244961815d1c0e9649c3b42a5aba4f5a
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:54:49 -0400

    Update mock_services.dart

--- test/mocks/mock_services.dart
+++ test/mocks/mock_services.dart
@@ -15,10 +15,16 @@ class MockFlutterTts extends Mock implements FlutterTts {
 
 class MockLocalAuth extends Mock implements LocalAuthentication {
   @override
-  Future<bool> authenticate({required String localizedReason, bool biometricOnly = false, bool useErrorDialogs = true, bool stickyAuth = false}) async => true;
+  Future<bool> authenticate({
+    required String localizedReason,
+    bool biometricOnly = false,
+    bool useErrorDialogs = true,
+    bool stickyAuth = false,
+    List<AuthMessages>? authMessages, // <--- Add this optional parameter!
+  }) async => true;
 
   @override
-  Future<bool> canCheckBiometrics() async => true;
+  Future<bool> get canCheckBiometrics async => true; // <-- FIXED: Now it's a getter!
 
   @override
   Future<bool> isDeviceSupported() async => true;

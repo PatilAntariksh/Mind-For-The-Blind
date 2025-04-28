3ba2d0fba1d78f957483072940ae385158a552ac
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 18:04:10 -0400

    Update login_test.dart

--- test/auth/login_test.dart
+++ test/auth/login_test.dart
@@ -6,14 +6,21 @@ import 'package:firebase_core/firebase_core.dart';
 void main() {
   setUpAll(() async {
     TestWidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "fake-api-key",
+        appId: "1:1234567890:web:abcdef123456",
+        messagingSenderId: "1234567890",
+        projectId: "test-project-id",
+      ),
+    );
   });
 
-  testWidgets('Login page renders properly', (WidgetTester tester) async {
+  testWidgets('Login page UI renders properly', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: LoginPage()));
 
     expect(find.byType(TextField), findsNWidgets(2)); // Email + Password
     expect(find.text('Login'), findsOneWidget);
-    expect(find.byIcon(Icons.fingerprint), findsOneWidget); // Biometrics button
+    expect(find.byIcon(Icons.fingerprint), findsOneWidget);
   });
 }

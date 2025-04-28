5e10a948589669d009df9d000f8157dcb3ec2295
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:26:40 -0400

    Update signup_test.dart

--- test/auth/signup_test.dart
+++ test/auth/signup_test.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:capstone_project/screens/signup_page.dart';
 
 void main() {
-  setUpAll(() async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
-  });
-
-  testWidgets('Signup screen has input fields and Sign Up button', (tester) async {
+  testWidgets('Signup page UI renders properly', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: SignUpPage()));
 
-    expect(find.byType(TextField), findsNWidgets(4)); // Name, Email, Password, Confirm
+  
+    expect(find.byType(TextField), findsNWidgets(4));
+
+  
     expect(find.text('Sign Up'), findsOneWidget);
-    expect(find.byType(ElevatedButton), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsWidgets);
   });
 }

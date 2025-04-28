1add0c6e2e22d393ddaaaf63eb687b4057ae8145
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:40:07 -0400

    Update login_test.dart

--- test/auth/login_test.dart
+++ test/auth/login_test.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:capstone_project/screens/login_page.dart';
+import '../mocks/mock_services.dart';
 
 void main() {
-  testWidgets('Login page UI renders properly', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(home: LoginPage()));
+  testWidgets('Login page UI renders properly', (tester) async {
+    final mockAuth = MockFirebaseAuth();
+    final mockTts = MockFlutterTts();
 
-    expect(find.byType(TextField), findsNWidgets(2)); 
+    await tester.pumpWidget(
+      MaterialApp(
+        home: LoginPage(),
+      ),
+    );
+
+    expect(find.byType(TextField), findsNWidgets(2));
     expect(find.text('Login'), findsOneWidget);
     expect(find.byIcon(Icons.fingerprint), findsOneWidget);
     expect(find.byType(ElevatedButton), findsWidgets);

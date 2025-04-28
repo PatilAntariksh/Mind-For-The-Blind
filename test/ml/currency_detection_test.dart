285da87594c0b3442ef17475d081382766c5347e
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:59:52 -0400

    Update currency_detection_test.dart

--- test/ml/currency_detection_test.dart
+++ test/ml/currency_detection_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('Currency detection placeholder test', () {
-    const result = '\$20';
-    expect(result.contains('\$'), isTrue);
+    expect(true, isTrue);
   });
 }

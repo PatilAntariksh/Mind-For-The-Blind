b0e3c6c64ac9859ce8cc4a0aaa0b71a8cf3670bc
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 18:20:20 -0400

    Update login_test.dart

--- test/auth/login_test.dart
+++ test/auth/login_test.dart
@@ -1,25 +1,17 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:capstone_project/screens/login_page.dart';
-import 'package:firebase_core/firebase_core.dart';
+import '../firebase_setup.dart';
 
 void main() {
   setUpAll(() async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp(
-      options: const FirebaseOptions(
-        apiKey: "fake-api-key",
-        appId: "1:1234567890:web:abcdef123456",
-        messagingSenderId: "1234567890",
-        projectId: "test-project-id",
-      ),
-    );
+    await initializeFirebase();
   });
 
   testWidgets('Login page UI renders properly', (WidgetTester tester) async {
     await tester.pumpWidget(const MaterialApp(home: LoginPage()));
 
-    expect(find.byType(TextField), findsNWidgets(2)); // Email + Password
+    expect(find.byType(TextField), findsNWidgets(2));
     expect(find.text('Login'), findsOneWidget);
     expect(find.byIcon(Icons.fingerprint), findsOneWidget);
   });

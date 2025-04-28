e8def3ae715508c8106d2418d6523ec5d05105a4
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:59:40 -0400

    Update video_test.dart

--- test/video/video_test.dart
+++ test/video/video_test.dart
@@ -1,11 +1,14 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:capstone_project/screens/video_call.dart';
 
 void main() {
-  testWidgets('VideoCallPage loads successfully with a room ID', (tester) async {
-    const testRoomId = 'demo-room';
-    await tester.pumpWidget(MaterialApp(home: VideoCallPage(roomId: testRoomId)));
+  testWidgets('VideoCallPage loads with room ID', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: VideoCallPage(roomId: 'testroom'),
+      ),
+    );
 
     expect(find.byType(VideoCallPage), findsOneWidget);
   });

84cca841365392a7914fb0cfe18d58569994a55c
PatilAntariksh <159646979+PatilAntariksh@users.noreply.github.com>
2025-04-28 17:27:02 -0400

    Update and rename login_test.dar to login_test.dart

---
+++ test/auth/login_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:capstone_project/screens/login_page.dart';
+
+void main() {
+  testWidgets('Login page UI renders properly', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginPage()));
+
+    expect(find.byType(TextField), findsNWidgets(2)); 
+    expect(find.text('Login'), findsOneWidget);
+    expect(find.byIcon(Icons.fingerprint), findsOneWidget);
+    expect(find.byType(ElevatedButton), findsWidgets);
+  });
+}
